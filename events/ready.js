03d662fc80b6f17f252087281ba8ffd8a6e42340
Lizzard <101275325+lizzard-development@users.noreply.github.com>
2025-04-26 16:46:18 +0300

    Create events/ready.js

---
+++ events/ready.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: 'ready',
+  once: true,
+  execute(client) {
+    const botName = client.user.tag;
+    const serverCount = client.guilds.cache.size;
+    const userCount = client.users.cache.size;
+
+    
+    client.user.setPresence({ activities: [{ name: 'ARES CODE'}], status: 'online' });
+
+    // Ready log
+    console.log(`✅ Bot hazır!`);
+    console.log(`Bot: ${botName}`);
+    console.log(`Sunucu Sayısı: ${serverCount}`);
+    console.log(`Kullanıcı Sayısı: ${userCount}`);
+  }
+
+
+
+
+// ARES CODE DİSCORD.JS V14 BOŞ ALTYAPI \\
